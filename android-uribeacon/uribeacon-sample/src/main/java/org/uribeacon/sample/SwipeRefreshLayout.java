e57066883a3a38e26472d2e2e42459daec6e5b00
Bill Schilit <schilit+github@gmail.com>
2014-11-23 19:50:41 -0800

    Run inspect code - cleanup

--- android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/SwipeRefreshLayout.java
+++ android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/SwipeRefreshLayout.java
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.uribeacon.sample;
 
-
-    import android.content.Context;
-    import android.util.AttributeSet;
-    import android.widget.ListView;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
 
 /**
  * Subclass of {@link android.support.v4.widget.SwipeRefreshLayout} that supports containing a
  * ViewGroup whose first child is a ListView. The ViewGroup can contain other views.
- *
  */
 public class SwipeRefreshLayout extends android.support.v4.widget.SwipeRefreshLayout {
 
@@ -20,7 +33,8 @@ public class SwipeRefreshLayout extends android.support.v4.widget.SwipeRefreshLa
     super(context, attributeSet);
   }
 
-  @Override public boolean canChildScrollUp() {
+  @Override
+  public boolean canChildScrollUp() {
     // The real child maps cares about is the list, so check if that can scroll.
     ListView target = (ListView) findViewById(android.R.id.list);
     return target.getChildCount() > 0
