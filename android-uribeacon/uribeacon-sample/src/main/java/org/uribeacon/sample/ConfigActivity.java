ee375d31c5319dbb65f278e3bfd58c1296e4daaf
Deqing Sun <iamseer@gmail.com>
2014-12-04 15:35:38 -0500

    Look for config UUID instead of assuming first one

--- android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/ConfigActivity.java
+++ android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/ConfigActivity.java
@@ -201,10 +201,15 @@ public class ConfigActivity extends Activity implements PasswordDialogFragment.P
         mConnectionDialog.show();
       }
       List<ParcelUuid> uuids = scanResult.getScanRecord().getServiceUuids();
-      // Assuming the first uuid is the config uuid
-      ParcelUuid uuid = uuids.get(0);
-      mUriBeaconConfig = new UriBeaconConfig(this, mUriBeaconCallback, uuid);
-      mUriBeaconConfig.connectUriBeacon(device);
+      // Check which uuid is the config uuid
+      ParcelUuid services[] = {ProtocolV1.CONFIG_SERVICE_UUID, ProtocolV2.CONFIG_SERVICE_UUID};
+      for (ParcelUuid serviceUuid : services){
+        int uuidIndex= uuids.indexOf(serviceUuid);
+        if (uuidIndex>=0) {
+          mUriBeaconConfig = new UriBeaconConfig(this, mUriBeaconCallback, uuids.get(uuidIndex));
+          mUriBeaconConfig.connectUriBeacon(device);
+        }
+      }
     }
   }
 
