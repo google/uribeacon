f88a30ac9a907fa81c326e61c42e525fbce0ab17
Bill Schilit <schilit+github@gmail.com>
2014-12-06 18:56:11 -0800

    Add cake and modes

---
+++ beacons/mbed/local-settings.sh
@@ -0,0 +1,2 @@
+export PATH=$PATH:/usr/local/google/gcc-arm-none-eabi-4_8-2014q3/bin
+
--- beacons/mbed/main.cpp
+++ beacons/mbed/main.cpp
@@ -21,33 +21,78 @@
 #include "DeviceInformationService.h"
 
 BLEDevice ble;
+URIBeaconConfigService *uriBeaconConfig;
 
 void disconnectionCallback(Gap::Handle_t handle, Gap::DisconnectionReason_t reason) {
   ble.startAdvertising();
 }
 
-int main(void) {
+void switchCallback(void)
+{
+  static bool switched = false;
+  if (!switched) {
+    printf("executing switch\r\n");
+    delete uriBeaconConfig;
+    uriBeaconConfig = NULL;
+ 
+    static const uint8_t BEACON_UUID[] = {0xD8, 0xFE};
+    static const uint8_t urldata[] = {
+      BEACON_UUID[0],
+      BEACON_UUID[1],
+      0x00, // flags
+      0x20, // power
+      0x00, // http://www.
+      'm',
+      'b',
+      'e',
+      'd',
+      0x08, // .".org"
+    };
+ 
+    ble.shutdown();
+    ble.init();
+ 
+    ble.clearAdvertisingPayload();
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, BEACON_UUID, sizeof(BEACON_UUID));
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA, urldata, sizeof(urldata));
+ 
+    ble.setAdvertisingType(GapAdvertisingParams::ADV_NON_CONNECTABLE_UNDIRECTED);
+    ble.setAdvertisingInterval(1600); /* 1s; in multiples of 0.625ms. */
+    ble.startAdvertising();
+ 
+    switched = true;
+  }
+}
+ 
+int main(void)
+{
+  Ticker ticker;
+  //  ticker.attach(switchCallback, 30);
+ 
   ble.init();
   ble.onDisconnection(disconnectionCallback);
-
-  URIBeaconConfigService uriBeaconConfig(ble, "http://uribeacon.org");
-  if (!uriBeaconConfig.configuredSuccessfully()) {
-    error("failed to create UriBeaconConfig service");
+ 
+  uriBeaconConfig = new URIBeaconConfigService(ble, "http://uribeacon.org");
+  if (!uriBeaconConfig->configuredSuccessfully()) {
+    error("failed to accommodate URI");
   }
-
-  /* Initialize UriBeaconConfig */
+  /* optional use of the API offered by URIBeaconConfigService */
   const int8_t powerLevels[] = {-20, -4, 0, 10};
-  uriBeaconConfig.setTxPowerLevels(powerLevels);
-  uriBeaconConfig.setTxPowerMode(URIBeaconConfigService::TX_POWER_MODE_LOW);
-
-  /* Setup optional services: other-the-air firmware updates and device information */
-  DFUService dfu(ble); 
-  DeviceInformationService deviceInfo(ble, "ARM", "UriBeacon", "SN1", "hw-rev1", "fw-rev1", "soft-rev1");
-
+  uriBeaconConfig->setTxPowerLevels(powerLevels);
+  uriBeaconConfig->setTxPowerMode(URIBeaconConfigService::TX_POWER_MODE_LOW);
+ 
+  /* Setup auxiliary services. */
+  DFUService dfu(ble); /* To allow over-the-air firmware udpates. optional. */
+  DeviceInformationService deviceInfo(ble, "ARM", "UriBeacon", "SN1", "hw-rev1", "fw-rev1", "soft-rev1"); /* optional */
+ 
+  ble.clearAdvertisingPayload();
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::INCOMPLETE_LIST_128BIT_SERVICE_IDS, URIBeacon2ControlServiceUUID, sizeof(URIBeacon2ControlServiceUUID));
+  ble.setAdvertisingInterval(Gap::MSEC_TO_ADVERTISEMENT_DURATION_UNITS(1000));
+  ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+  ble.setAdvertisingInterval(1600); /* 1000ms; in multiples of 0.625ms. */
   ble.startAdvertising();
-
+ 
   while (true) {
     ble.waitForEvent();
   }
 }
-  
