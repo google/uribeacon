bc8a96644db716ca3e19f9edd35671d2e7cc5145
Giovanni Ortuño <gd.ortuno@gmail.com>
2015-01-16 09:21:31 -0800

    Code -> Reformat Code

--- android-uribeacon/uribeacon-validator/src/main/java/org/uribeacon/validator/TestAction.java
+++ android-uribeacon/uribeacon-validator/src/main/java/org/uribeacon/validator/TestAction.java
@@ -19,6 +19,7 @@ package org.uribeacon.validator;
 import java.util.UUID;
 
 public class TestAction {
+
   public final static int CONNECT = 0;
   public final static int WRITE = 1;
   public final static int ASSERT = 2;
@@ -41,7 +42,8 @@ public class TestAction {
     this.actionType = type;
   }
 
-  public TestAction(int actionType, UUID characteristicUuid, int expectedReturnCode, byte[] transmittedValue) {
+  public TestAction(int actionType, UUID characteristicUuid, int expectedReturnCode,
+      byte[] transmittedValue) {
     this.actionType = actionType;
     this.characteristicUuid = characteristicUuid;
     this.expectedReturnCode = expectedReturnCode;
