4d4eb56f7abf8e8f4c157e36055e7aae48238529
Giovanni Ortuño <gd.ortuno@gmail.com>
2015-02-12 12:52:59 -0800

    Removed unnecessary listeners

--- android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/SettingsActivity.java
+++ android-uribeacon/uribeacon-sample/src/main/java/org/uribeacon/sample/SettingsActivity.java
@@ -17,67 +17,16 @@ package org.uribeacon.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
 
 public class SettingsActivity extends Activity {
 
-  public static class SettingsFragment extends PreferenceFragment implements
-      OnPreferenceChangeListener {
+  public static class SettingsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       addPreferencesFromResource(R.xml.pref_general);
-      // Establish listeners for scan mode list preference change
-      setSummaryChangeListener(R.string.pref_key_serial_scan_mode);
-    }
-
-    // Change the summary of the preference. Allow null new value for initialization.
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-      if (preference instanceof SwitchPreference) {
-        SwitchPreference s = (SwitchPreference) preference;
-
-        if (newValue != null) {
-          s.setChecked(((Boolean) newValue));
-        }
-      } else if (preference instanceof EditTextPreference) {
-        EditTextPreference e = (EditTextPreference) preference;
-        if (newValue != null) {
-          e.setText(newValue.toString());
-        }
-        preference.setSummary(e.getText());
-      } else if (preference instanceof ListPreference) {
-        ListPreference l = (ListPreference) preference;
-        if (newValue != null) {
-          l.setValue(newValue.toString());
-        }
-        preference.setSummary(l.getEntry());
-      } else if (preference instanceof CheckBoxPreference) {
-        CheckBoxPreference c = (CheckBoxPreference) preference;
-        if (newValue != null) {
-          c.setChecked(((Boolean) newValue));
-        }
-      }
-
-      return true;
-    }
-
-    // Initialize the preference summary and register a change listener.
-    private void setSummaryChangeListener(int resId) {
-      Preference preference = findPreference(getString(resId));
-      onPreferenceChange(preference, null);
-      preference.setOnPreferenceChangeListener(this);
-    }
-
-    private Preference getPreference(int id) {
-      return findPreference(getString(id));
     }
   }
 
